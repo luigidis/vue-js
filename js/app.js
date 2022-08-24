53120d37607ead493b4b82f25e3cfe8d7aa71c62
luigidis <luigi.distefano69@gmail.com>
2022-08-24 12:03:19 +0200

    Lezione 2 di Vue finita

--- js/app.js
+++ js/app.js
@@ -1,28 +1,87 @@
+// const app = new Vue ({
+//     el: '#root',
+//     data: {
+//         message: 'Ciao mamma',
+//         titolo: 'Benvenuti su Vue!!',
+//         classiParagrafo: 'bg-red testo-piccolo border-blue',
+//         idTitolo: 'main-title',
+//     },
+//     methods: {
+//         saluta: function () {
+//             console.log('ciao')
+//             console.log(this.message)
+//         },
+//         toUpper(text) {
+//             return text.toUpperCase()
+//         },
+//         toggleClass() {
+//             console.log('classi' + this.classiParagrafo)
+//             if (this.classiParagrafo === 'bg-red') {
+//                 this.classiParagrafo = 'bg-blue'
+//             } else {
+//                 this.classiParagrafo = 'bg-red'
+//             }
+//         }
+//     }
+// })
+
+// console.log(app)
+
 const app = new Vue ({
     el: '#root',
     data: {
-        message: 'Ciao mamma',
-        titolo: 'Benvenuti su Vue!!',
-        classiParagrafo: 'bg-red testo-piccolo border-blue',
-        idTitolo: 'main-title',
+        title: 'ciao mondo',
+        show: false,
+        imgSrc: 'https://picsum.photos/200/300',
+        nomi: [
+            'Gianluca',
+            'Marco',
+            'Mimmo',
+            'Pasquale',
+            'Geronimo',
+        ],
+        user: {
+            name: 'Gianluca',
+            lastName: 'Lomarco',
+            weight: 85,
+            height: 175,
+            jobTitle: 'Developper',
+        },
+        todos: [
+            {
+                text: 'Fai questo',
+                done: false,
+            },
+            {
+                text: 'Fai questo',
+                done: false,
+            },
+            {
+                text: 'Ora questo',
+                done: true,
+            },
+            {
+                text: 'Poi quello',
+                done: true,
+            },
+            {
+                text: 'Infine questo',
+                done: false,
+            },
+        ],
     },
     methods: {
-        saluta: function () {
-            console.log('ciao')
-            console.log(this.message)
-        },
-        toUpper(text) {
-            return text.toUpperCase()
-        },
-        toggleClass() {
-            console.log('classi' + this.classiParagrafo)
-            if (this.classiParagrafo === 'bg-red') {
-                this.classiParagrafo = 'bg-blue'
+        onToggle () {
+            if(this.show) {
+                this.show = false
             } else {
-                this.classiParagrafo = 'bg-red'
+                this.show = true
             }
-        }
+            }
+            
     }
 })
 
+
 console.log(app)
+
